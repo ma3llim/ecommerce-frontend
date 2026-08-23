5a5e7dc048271955595d3c04bc77ddcdd3ebeced
ma3llim <mohdsameer68257@gmail.com>
2026-08-24 00:19:00 +0530

    feat: update TypeScript configuration and Vite alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            "@": import.meta.dirname + "src",
+            "@": path.resolve(import.meta.dirname, "./src"),
         },
     },
 });
