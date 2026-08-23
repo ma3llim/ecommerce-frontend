f578c302f9bd298a330096cdc063673077c269d3
ma3llim <mohdsameer68257@gmail.com>
2026-08-23 23:17:46 +0530

    feat: add ResponsiveViewer component for responsive design display

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import QueryProvider from "./query/QueryProvider.tsx";
 import ThemeProvider from "./providers/ThemeProvider.tsx";
+import ResponsiveViewer from "./components/ResponsiveViewer.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider>
             <QueryProvider>
                 <App />
+                <ResponsiveViewer />
             </QueryProvider>
         </ThemeProvider>
     </StrictMode>
