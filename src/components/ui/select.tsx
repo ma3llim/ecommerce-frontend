e2f668c09b8cd465883ec2e4c852af6d9864b67f
ma3llim <mohdsameer68257@gmail.com>
2026-08-26 00:19:45 +0530

    fix: update import path for utility function in Select component

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { Select as SelectPrimitive } from "@base-ui/react/select";
 
-import { cn } from "src/lib/utils";
 import { ChevronDownIcon, CheckIcon, ChevronUpIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const Select = SelectPrimitive.Root;
 
